4a4595eb29280c29b8196041004e9a922c39f660
Lê Văn Thiện <22dh113465@st.huflit.edu.vn>
2024-10-18 11:45:13 +0700

    QLSanPham

--- TSWeb/Controllers/HomeController.cs
+++ TSWeb/Controllers/HomeController.cs
@@ -12,42 +12,7 @@ namespace TSWeb.Controllers
         {
             return View();
         }
-        public ActionResult Cart()
-        {
-            return View();
-        }
-        public ActionResult Chitiet()
-        {
-            return View();
-        }
-        public ActionResult Payment()
-        {
-            return View();
-        }
-        public ActionResult TongQuan()
-        {
-            return View();
-        }
-        public ActionResult KhachHang()
-        {
-            return View();
-        }
-        public ActionResult NhanVien()
-        {
-            return View();
-        }
-        public ActionResult SanPham()
-        {
-            return View();
-        }
-        public ActionResult ThanhToan()
-        {
-            return View();
-        }
-        public ActionResult UuDai()
-        {
-            return View();
-        }
+      
        
     }
 }
--- TSWeb/Controllers/SanPhamController.cs
+++ TSWeb/Controllers/SanPhamController.cs
@@ -27,5 +27,41 @@ namespace TSWeb.Controllers
         {
             return View("XoaSanPham");
         }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Chitiet()
+        {
+            return View();
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        public ActionResult TongQuan()
+        {
+            return View();
+        }
+        public ActionResult KhachHang()
+        {
+            return View();
+        }
+        public ActionResult NhanVien()
+        {
+            return View();
+        }
+        public ActionResult QLSanPham()
+        {
+            return View();
+        }
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+        public ActionResult UuDai()
+        {
+            return View();
+        }
     }
 }
