22136a596818a6a059db2bb090551a5c51f46aed
NganHa9797 <hahoang041104@gmail.com>
2024-10-24 07:27:33 +0700

    themsp

--- TSWeb/Models/DatabaseModel.cs
+++ TSWeb/Models/DatabaseModel.cs
@@ -7,22 +7,27 @@ using System.Collections;
 namespace TSWeb.Models {
     public class DatabaseModel {
         private string connecttionStrings = "workstation id=TSWebDaTa.mssql.somee.com;packet size=4096;user id=Vanh_SQLLogin_1;pwd=123456789;data source=TSWebDaTa.mssql.somee.com;persist security info=False;initial catalog=TSWebDaTa;TrustServerCertificate=True";
-        public ArrayList get(String sql) {
-            ArrayList datalist = new ArrayList();
-            SqlConnection connection = new SqlConnection(connecttionStrings);
-            SqlCommand command = new SqlCommand(sql, connection);
-            connection.Open();
-            using (SqlDataReader r = command.ExecuteReader()) {
-                while (r.Read()) {
-                    ArrayList row = new ArrayList();
-                    for (int i = 0; i < r.FieldCount; i++) {
-                        row.Add(r.GetValue(i).ToString());
-                    }
-                    datalist.Add(row);
+        public List<object> get(string sql, SqlParameter[] parameters = null) {
+            List<object> datalist = new List<object>(); // Sử dụng List<object> thay vì ArrayList
+            using (SqlConnection connection = new SqlConnection(connecttionStrings)) {
+                SqlCommand command = new SqlCommand(sql, connection);
 
+                // Thêm các tham số vào câu lệnh nếu có
+                if (parameters != null) {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        List<object> row = new List<object>();
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            row.Add(reader.GetValue(i));
+                        }
+                        datalist.Add(row);
+                    }
                 }
             }
-            connection.Close();
             return datalist;
         }
     }
