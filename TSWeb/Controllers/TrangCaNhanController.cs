d50ac427decd67e6dc8a02c5efda507cb63c3a70
ngoctb <tranbaongoc72004@example.com>
2024-10-28 18:34:16 +0700

    upload sources code

---
+++ TSWeb/Controllers/TrangCaNhanController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+using TSWeb.Models;
+
+namespace TSWeb.Controllers
+{
+    public class TrangCaNhanController : Controller
+    {
+        // GET: TrangCaNhan
+        DatabaseModel db = new DatabaseModel();
+        public ActionResult TrangCaNhan()
+        {
+            ViewBag.list = db.get("EXEC XemNguoiDungTheoID 1;");
+            return View();
+        }
+        public ActionResult Donhang()
+        {
+            return View();
+        }
+        public ActionResult Lichsudonhhang()
+        {
+            return View();
+        }
+        public ActionResult Voucher()
+        {
+            return View();
+        }
+        public ActionResult Diemtichluy()
+        {
+            return View();
+        }
+    }
+}
