332eb5deaa355a47b80514da485e603ac1d8d5b6
anh-le-24 <anhle241003@gmail.com>
2024-11-07 03:40:18 +0700

    update giỏ hàng

--- TSWeb/Controllers/HomeController.cs
+++ TSWeb/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,7 +29,43 @@ namespace TSWeb.Controllers
             ViewBag.list = db.get("EXEC HienThiSanPhamTheoID " + idsp + ";");
             return View();
         }
- 
+
+        [HttpPost]
+        public ActionResult ThemGH(string idnd, string idsp, int SoLuong, int? TongTien, int? tongtientp, string size, string selectedOptions)
+        {
+            try
+            {
+                // Kiểm tra giá trị đầu vào
+                if (string.IsNullOrEmpty(idnd) || string.IsNullOrEmpty(idsp) || string.IsNullOrEmpty(size))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                // Xây dựng câu lệnh SQL với các giá trị nhập từ người dùng
+                string sqlQuery = "EXEC ThemGioHang " +
+                   "@IDKH = " + idnd + ", " +
+                   "@SoLuong = " + SoLuong + ", " +
+                   "@Gia = " + TongTien + ", " +
+                   "@Size = N'" + size + "', " +
+                   "@IDSP = " + idsp + ", " +
+                   "@DsTopping = N'" + selectedOptions + "', " +
+                   "@TongTienTopping = " + tongtientp + ";";
+
+                // Thực thi câu lệnh SQL
+                db.get(sqlQuery);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ghi log hoặc thông báo cho người dùng)
+                Console.WriteLine(ex.Message);
+                return RedirectToAction("ChiTiet", "Home", new {idsp = idsp});
+            }
+
+            return RedirectToAction("ListSanPham", "Home");
+        }
+
+
+
         public ActionResult ThanhToan()
         {
             return View();
